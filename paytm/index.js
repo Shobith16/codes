9ede87a778ee509f8b418fd1fc375d7ab49f51a2
Shobith Acharya <shobithracharya1816@gmail.com>
2024-01-03 08:18:02 +0530

    adding two files

---
+++ paytm/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+const port = 3000
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}` );
+})
